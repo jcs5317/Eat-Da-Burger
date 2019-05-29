04633647048ee97c277dd3d47664863d67841c98
jordan <jcs5317@gmail.com>
2019-05-28 17:43:44 -0700

    add image

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "password",
-  database: "burger_db123"
+  password: "root",
+  database: "burger_db"
 });
 
 // Make connection.
